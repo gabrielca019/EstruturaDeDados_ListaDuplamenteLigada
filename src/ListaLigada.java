3b5724e76cfaf8c3472983909a6ab21ec46a3fed
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2023-08-09 21:23:53 -0300

    Refatoração e clean code codigo

--- src/ListaLigada.java
+++ src/ListaLigada.java
@@ -1,22 +1,22 @@
 
 public class ListaLigada {
 	
-	private Celula primeira = null;
-	private Celula ultima = null;
-	private int totalDeElementos = 0;
+	private Celula primeiraCelula = null;
+	private Celula ultimaCelula = null;
+	private int totalDeCelulas = 0;
 	
 	public void adicionarElementoNoInicio(Object novoElemento) {
 		if (verificarListaEstaVazia()) {
-			Celula nova = new Celula(novoElemento);
-			this.primeira = nova;
-			this.ultima = nova;
+			Celula novaCelula = new Celula(novoElemento);
+			this.primeiraCelula = novaCelula;
+			this.ultimaCelula = novaCelula;
 		}
 		else {
-			Celula nova = new Celula(novoElemento, this.primeira);
-			this.primeira.setAnterior(nova);
-			this.primeira = nova;
+			Celula novaCelula = new Celula(novoElemento, this.primeiraCelula);
+			this.primeiraCelula.setAnterior(novaCelula);
+			this.primeiraCelula = novaCelula;
 		}
-		this.totalDeElementos++;
+		this.totalDeCelulas++;
 	}
 	
 	public void adicionarElementoNoFinal(Object novoElemento) {
@@ -24,104 +24,93 @@ public class ListaLigada {
 			adicionarElementoNoInicio(novoElemento);
 		}
 		else {
-			Celula nova = new Celula(novoElemento);
-			this.ultima.setProximo(nova);
-			nova.setAnterior(this.ultima);
-			this.ultima = nova;
-			this.totalDeElementos++;
+			Celula novaCelula = new Celula(novoElemento);
+			this.ultimaCelula.setProximo(novaCelula);
+			novaCelula.setAnterior(this.ultimaCelula);
+			this.ultimaCelula = novaCelula;
+			this.totalDeCelulas++;
 		}
 		
 	}
 	
-	private boolean posicaoValidaInsercao(int posicao) {
-		return (posicao >= 0) && (posicao <= this.totalDeElementos);
+	private boolean verificarPosicaoValidaParaInsercao(int posicao) {
+		return (posicao >= 0) && (posicao <= this.totalDeCelulas);
 	}
 	
-	public void adicinaNaPosicao(Object novoElemento, int posicao) {
+	public void adicionarNaPosicaoEspecifica(Object novoElemento, int posicao) {
+		if (!verificarPosicaoValidaParaInsercao(posicao)) 
+			throw new IllegalArgumentException("Posição Inválida!");
 		
-		if (!posicaoValidaInsercao(posicao)) {
-			throw new IllegalArgumentException("Posicao Invalida");
-		}
 		
 		if (posicao == 0) {
 			adicionarElementoNoInicio(novoElemento);
 		}
-		else if (posicao == this.totalDeElementos) {
+		else if (posicao == this.totalDeCelulas) {
 			adicionarElementoNoFinal(novoElemento);
 		}
 		else {
-			Celula anterior = pegaCelula(posicao - 1);
-			Celula proxima = anterior.getProximo();
+			Celula celulaAnterior = getCelulaNaPosicaoEspecifica(posicao - 1);
+			Celula celulaSucessora = celulaAnterior.getProximo();
 			
-			Celula nova = new Celula(novoElemento, proxima);
-			nova.setAnterior(anterior);
-			anterior.setProximo(nova);
-			proxima.setAnterior(nova);
-			this.totalDeElementos++;
+			Celula novaCelula = new Celula(novoElemento, celulaSucessora);
+			novaCelula.setAnterior(celulaAnterior);
+			celulaAnterior.setProximo(novaCelula);
+			celulaSucessora.setAnterior(novaCelula);
+			this.totalDeCelulas++;
 		}
-		
 	}
 	
 	private boolean verificarListaEstaVazia() {
-	    return  totalDeElementos == 0;
-	
+	    return totalDeCelulas == 0;
 	}
 	
-	public int pegaTotalElementos() {
-		return totalDeElementos;
+	public int getTotalCelulas() {
+		return totalDeCelulas;
 	}
 	
-	public Object pegaPrimeiro() {
-		if(this.primeira == null) 
+	public Object getPrimeiraCelula() {
+		if(this.primeiraCelula == null) 
 			throw new IllegalArgumentException("Primeira posição está vazia!");
 		
-		return this.primeira.getElemento();
+		return this.primeiraCelula.getElemento();
 	}
 	
-	public Object pegaUltima() {
-		if(this.ultima == null) 
+	public Object getUltimaCelula() {
+		if(this.ultimaCelula == null) 
 			throw new IllegalArgumentException("Ultima posição está vazia!");
 		
-		
-		return this.ultima.getElemento();
+		return this.ultimaCelula.getElemento();
 	}
 	
 	private boolean posicaoValidaRecuperar(int posicao) {
-		return (posicao >= 0) && (posicao < this.totalDeElementos);
+		return (posicao >= 0) && (posicao < this.totalDeCelulas);
 	}
 	
-	private Celula pegaCelula(int posicao) {
-		
-		if (!posicaoValidaRecuperar(posicao)) {
+	private Celula getCelulaNaPosicaoEspecifica(int posicao) {
+		if (!posicaoValidaRecuperar(posicao)) 
 			throw new IllegalArgumentException("Posicao Invalida");
-		}
 		
-		Celula atual = this.primeira;
+		Celula celulaAtual = this.primeiraCelula;
 		
-		for (int i=0; i < posicao; i++) {
-			atual = atual.getProximo();
-		}
+		for (int i=0; i < posicao; i++) 
+			celulaAtual = celulaAtual.getProximo();
 		
-		return atual;
+		return celulaAtual;
 			
 	}
 	
-	public Object pega(int posicao) {
-		
-		return pegaCelula(posicao).getElemento();
-		
+	public Object getElementoPosicaoEspecifica(int posicao) {
+		return getCelulaNaPosicaoEspecifica(posicao).getElemento();
 	}
 	
 	public boolean contem (Object elementoBuscado) {
+		Celula celulaAtual = this.primeiraCelula;
 		
-		Celula atual = this.primeira;
-		
-		while (atual != null) {
-			
-			if (atual.getElemento().equals(elementoBuscado)) {
+		while (celulaAtual != null) {
+			if (celulaAtual.getElemento().equals(elementoBuscado)) 
 				return true;
-			}
-			atual = atual.getProximo();
+			
+			celulaAtual = celulaAtual.getProximo();
 		}
 		
 		return false;
@@ -131,26 +120,26 @@ public class ListaLigada {
 		if(verificarListaEstaVazia()) 
 			throw new IllegalArgumentException("Remoção inválida pela lista estar vazia!");
 		
-		if(this.primeira == this.ultima) {
-			this.primeira = null;
-			this.ultima = null;
+		if(this.primeiraCelula == this.ultimaCelula) {
+			this.primeiraCelula = null;
+			this.ultimaCelula = null;
 		} else {
-			this.primeira = this.primeira.getProximo();
-			this.primeira.setAnterior(null);
+			this.primeiraCelula = this.primeiraCelula.getProximo();
+			this.primeiraCelula.setAnterior(null);
 		}
-		totalDeElementos--;
+		totalDeCelulas--;
 	}
 	
 	public void removerCelulaFinal() {
 		if(verificarListaEstaVazia()) 
 			throw new IllegalArgumentException("Remoção inválida pela lista estar vazia!");
 		
-		if(this.primeira == this.ultima) {
+		if(this.primeiraCelula == this.ultimaCelula) {
 			removerCelulaInicial();
 		} else {
-			this.ultima = this.ultima.getAnterior();
-			this.ultima.setProximo(null);
-			totalDeElementos--;
+			this.ultimaCelula = this.ultimaCelula.getAnterior();
+			this.ultimaCelula.setProximo(null);
+			totalDeCelulas--;
 		}
 	}
 	
@@ -160,15 +149,13 @@ public class ListaLigada {
 		
 		if(posicao == 0) {
 			removerCelulaInicial();
-		} else if(posicao == (totalDeElementos - 1)) {
+		} else if(posicao == (totalDeCelulas - 1)) {
 			removerCelulaFinal();
 		} else {
-			Celula celulaRemovida = this.pegaCelula(posicao);
+			Celula celulaRemovida = this.getCelulaNaPosicaoEspecifica(posicao);
 			celulaRemovida.getAnterior().setProximo(celulaRemovida.getProximo());
 			celulaRemovida.getProximo().setAnterior(celulaRemovida.getAnterior());
-			totalDeElementos--;
+			totalDeCelulas--;
 		}
-		
 	}
-
 }
--- src/ListaLigadaTest.java
+++ src/ListaLigadaTest.java
@@ -1,316 +1,246 @@
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ListaLigadaTest {
-
-	@Test
-	void testAdicionarNoComecoListaVazia() {
-
-		ListaLigada lista = new ListaLigada();
-
-		lista.adicionarElementoNoInicio("Fernando");
-
-		assertEquals(1, lista.pegaTotalElementos());
-		assertEquals("Fernando", lista.pegaPrimeiro());
-		assertEquals("Fernando", lista.pegaUltima());		
+	
+	ListaLigada lista;
+	
+	@BeforeEach
+	void criarLista() {
+		lista = new ListaLigada();
 	}
 
 	@Test
-	void testAdicionarNoComecoComElementos() {
+	void testAdicionarNoComecoListaVazia() {
+		lista.adicionarElementoNoInicio("Fernando");
 
-		ListaLigada lista = new ListaLigada();
+		assertEquals(1, lista.getTotalCelulas());
+		assertEquals("Fernando", lista.getPrimeiraCelula());
+		assertEquals("Fernando", lista.getUltimaCelula());		
+	}
 
+	@Test
+	void testAdicionarNoInicioComElementos() {
 		lista.adicionarElementoNoInicio("Fernando");
 		lista.adicionarElementoNoInicio("Maria");
 		lista.adicionarElementoNoInicio("Carlos");
 
-		assertEquals(3, lista.pegaTotalElementos());
-		assertEquals("Carlos", lista.pegaPrimeiro());
-		assertEquals("Fernando", lista.pegaUltima());
+		assertEquals(3, lista.getTotalCelulas());
+		assertEquals("Carlos", lista.getPrimeiraCelula());
+		assertEquals("Fernando", lista.getUltimaCelula());
 
 	}
 
 	@Test
 	void testAdicionarNoFinalListaVazia() {
-		
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 
-		assertEquals(1, lista.pegaTotalElementos());
-		assertEquals("Fernando", lista.pegaPrimeiro());
-		assertEquals("Fernando", lista.pegaUltima());
-
-
+		assertEquals(1, lista.getTotalCelulas());
+		assertEquals("Fernando", lista.getPrimeiraCelula());
+		assertEquals("Fernando", lista.getUltimaCelula());
 	}
 
 	@Test
 	void testAdicionarNoFinalComElementos() {
-
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 		lista.adicionarElementoNoFinal("Maria");
 		lista.adicionarElementoNoFinal("Carlos");
 
-		assertEquals(3, lista.pegaTotalElementos());
-		assertEquals("Fernando", lista.pegaPrimeiro());
-		assertEquals("Carlos", lista.pegaUltima());
-
+		assertEquals(3, lista.getTotalCelulas());
+		assertEquals("Fernando", lista.getPrimeiraCelula());
+		assertEquals("Carlos", lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testPegarNaPosicaoInvalidaNegativa() {
-		
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 		
-		assertThrows(IllegalArgumentException.class, ()->lista.pega(-1));
-		
+		assertThrows(IllegalArgumentException.class, ()->lista.getElementoPosicaoEspecifica(-1));
 	}
 	
 	@Test
 	void testPegarNaPosicaoInvalidaQuantidadeElementos() {
-	
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 		
-		assertThrows(IllegalArgumentException.class, ()->lista.pega(6));
+		assertThrows(IllegalArgumentException.class, ()->lista.getElementoPosicaoEspecifica(6));
 	}
 	
 	@Test
 	void testPegarNaPosicaoValida() {
-		
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 		lista.adicionarElementoNoFinal("Maria");
 		lista.adicionarElementoNoFinal("Carlos");
 
-		assertEquals("Fernando", lista.pega(0));
-		assertEquals("Maria", lista.pega(1));
-		assertEquals("Carlos", lista.pega(2));
-		
-		
+		assertEquals("Fernando", lista.getElementoPosicaoEspecifica(0));
+		assertEquals("Maria", lista.getElementoPosicaoEspecifica(1));
+		assertEquals("Carlos", lista.getElementoPosicaoEspecifica(2));
 	}
 	
 	@Test
 	void testAdicionarNaPosicaoInvalidaNegativa() {
-		
-		ListaLigada lista = new ListaLigada();
-		
 		lista.adicionarElementoNoFinal("Carlos");
 		
-		assertThrows(IllegalArgumentException.class, ()->lista.adicinaNaPosicao("Fernando", -1));
-		
+		assertThrows(IllegalArgumentException.class, ()->lista.adicionarNaPosicaoEspecifica("Fernando", -1));
 	}
 	
 	@Test
 	void testAdicionarNaPosicaoInvalidaQuantidadeElementos() {
-	
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Carlos");
 		
-		assertThrows(IllegalArgumentException.class, ()->lista.adicinaNaPosicao("Fernando", 2));
-		
+		assertThrows(IllegalArgumentException.class, ()->lista.adicionarNaPosicaoEspecifica("Fernando", 2));
 	}
 	
 	@Test
 	void testAdicionarNaPosicaoValida() {
-		
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 		lista.adicionarElementoNoFinal("Maria");
 		lista.adicionarElementoNoFinal("Carlos");
 
-		lista.adicinaNaPosicao("Rose", 1);
+		lista.adicionarNaPosicaoEspecifica("Rose", 1);
 		
-		assertEquals("Fernando", lista.pega(0));
-		assertEquals("Rose", lista.pega(1));
-		assertEquals("Maria", lista.pega(2));
-		assertEquals("Carlos", lista.pega(3));
+		assertEquals("Fernando", lista.getElementoPosicaoEspecifica(0));
+		assertEquals("Rose", lista.getElementoPosicaoEspecifica(1));
+		assertEquals("Maria", lista.getElementoPosicaoEspecifica(2));
+		assertEquals("Carlos", lista.getElementoPosicaoEspecifica(3));
 		
 	}
 	
 	@Test
 	void testContemListaVazia() {
-		
-		ListaLigada lista = new ListaLigada();
-		
 		assertFalse(lista.contem("Mauro"));
-		
 	}
 	
 	@Test
 	void testContemNaoAcharLista() {
-		
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 		lista.adicionarElementoNoFinal("Maria");
 		lista.adicionarElementoNoFinal("Carlos");
 		
 		assertFalse(lista.contem("Mauro"));
-		
 	}
 	
 	@Test
 	void testContemAcharLista() {
-		
-		ListaLigada lista = new ListaLigada();
-
 		lista.adicionarElementoNoFinal("Fernando");
 		lista.adicionarElementoNoFinal("Maria");
 		lista.adicionarElementoNoFinal("Carlos");
 		
 		assertTrue(lista.contem("Maria"));
-		
-		
 	}
 	
 	/* REMOVER INICIO */
 	
 	@Test
 	void testRemoverCelulaInicialListaVazia() {
-		ListaLigada lista = new ListaLigada();
-		
 		assertThrows(IllegalArgumentException.class, () -> lista.removerCelulaInicial());
-		assertEquals(0, lista.pegaTotalElementos());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaPrimeiro());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaUltima());
+		assertEquals(0, lista.getTotalCelulas());
+		assertThrows(IllegalArgumentException.class, () -> lista.getPrimeiraCelula());
+		assertThrows(IllegalArgumentException.class, () -> lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testRemoverCelulaInicialListaUmElemento() {
-		ListaLigada lista = new ListaLigada();
 		lista.adicionarElementoNoInicio("Gabriel");
-		
-		assertEquals(1, lista.pegaTotalElementos());
+		assertEquals(1, lista.getTotalCelulas());
 		
 		lista.removerCelulaInicial();
-		
-		assertEquals(0, lista.pegaTotalElementos());
+		assertEquals(0, lista.getTotalCelulas());
 		assertThrows(IllegalArgumentException.class, () -> lista.removerCelulaInicial());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaPrimeiro());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaUltima());
+		assertThrows(IllegalArgumentException.class, () -> lista.getPrimeiraCelula());
+		assertThrows(IllegalArgumentException.class, () -> lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testRemoverCelulaInicialListaVariosElementos() {
-		ListaLigada lista = new ListaLigada();
 		lista.adicionarElementoNoInicio("Gabriel");
 		lista.adicionarElementoNoInicio("Matheus");
 		lista.adicionarElementoNoInicio("Paulo");
-		
-		assertEquals(3, lista.pegaTotalElementos());
+		assertEquals(3, lista.getTotalCelulas());
 		
 		lista.removerCelulaInicial();
-		
-		assertEquals(2, lista.pegaTotalElementos());
-		assertEquals("Matheus", lista.pega(0));
-		
-		assertEquals("Matheus", lista.pegaPrimeiro());
-		assertEquals("Gabriel", lista.pegaUltima());
+		assertEquals(2, lista.getTotalCelulas());
+		assertEquals("Matheus", lista.getElementoPosicaoEspecifica(0));
+		assertEquals("Matheus", lista.getPrimeiraCelula());
+		assertEquals("Gabriel", lista.getUltimaCelula());
 	}
 	
 	/* REMOVER FIM */
 	
 	@Test
 	void testRemoverCelulaFinallListaVazia() {
-		ListaLigada lista = new ListaLigada();
-		
 		assertThrows(IllegalArgumentException.class, () -> lista.removerCelulaFinal());
-		assertEquals(0, lista.pegaTotalElementos());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaPrimeiro());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaUltima());
+		assertEquals(0, lista.getTotalCelulas());
+		assertThrows(IllegalArgumentException.class, () -> lista.getPrimeiraCelula());
+		assertThrows(IllegalArgumentException.class, () -> lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testRemoverCelulaFinalListaUmElemento() {
-		ListaLigada lista = new ListaLigada();
 		lista.adicionarElementoNoFinal("Gabriel");
-		
-		assertEquals(1, lista.pegaTotalElementos());
+		assertEquals(1, lista.getTotalCelulas());
 		
 		lista.removerCelulaFinal();
-		
-		assertEquals(0, lista.pegaTotalElementos());
+		assertEquals(0, lista.getTotalCelulas());
 		assertThrows(IllegalArgumentException.class, () -> lista.removerCelulaFinal());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaPrimeiro());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaUltima());
+		assertThrows(IllegalArgumentException.class, () -> lista.getPrimeiraCelula());
+		assertThrows(IllegalArgumentException.class, () -> lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testRemoverCelulaFinalListaVariosElementos() {
-		ListaLigada lista = new ListaLigada();
 		lista.adicionarElementoNoFinal("Gabriel");
 		lista.adicionarElementoNoFinal("Matheus");
 		lista.adicionarElementoNoFinal("Paulo");
-		
-		assertEquals(3, lista.pegaTotalElementos());
+		assertEquals(3, lista.getTotalCelulas());
 		
 		lista.removerCelulaFinal();
-		
-		assertEquals(2, lista.pegaTotalElementos());
-		assertEquals("Matheus", lista.pega(1));
-		assertEquals("Gabriel", lista.pegaPrimeiro());
-		assertEquals("Matheus", lista.pegaUltima());
+		assertEquals(2, lista.getTotalCelulas());
+		assertEquals("Matheus", lista.getElementoPosicaoEspecifica(1));
+		assertEquals("Gabriel", lista.getPrimeiraCelula());
+		assertEquals("Matheus", lista.getUltimaCelula());
 	}
 	
 	/* REMOVER NA POSICAO X */
 	
 	@Test
 	void testRemoverCelulaXlListaVazia() {
-		ListaLigada lista = new ListaLigada();
-		
 		assertThrows(IllegalArgumentException.class, () -> lista.removerCelulaPosicaoEspecifica(0));
-		assertEquals(0, lista.pegaTotalElementos());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaPrimeiro());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaUltima());
+		assertEquals(0, lista.getTotalCelulas());
+		assertThrows(IllegalArgumentException.class, () -> lista.getPrimeiraCelula());
+		assertThrows(IllegalArgumentException.class, () -> lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testRemoverCelulaXListaUmElemento() {
-		ListaLigada lista = new ListaLigada();
 		lista.adicionarElementoNoFinal("Gabriel");
-		
-		assertEquals(1, lista.pegaTotalElementos());
+		assertEquals(1, lista.getTotalCelulas());
 		
 		lista.removerCelulaPosicaoEspecifica(0);
-		
-		assertEquals(0, lista.pegaTotalElementos());
+		assertEquals(0, lista.getTotalCelulas());
 		assertThrows(IllegalArgumentException.class, () -> lista.removerCelulaPosicaoEspecifica(0));
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaPrimeiro());
-		assertThrows(IllegalArgumentException.class, () -> lista.pegaUltima());
+		assertThrows(IllegalArgumentException.class, () -> lista.getPrimeiraCelula());
+		assertThrows(IllegalArgumentException.class, () -> lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testRemoverCelulaXListaVariosElementos() {
-		ListaLigada lista = new ListaLigada();
 		lista.adicionarElementoNoFinal("Gabriel");
 		lista.adicionarElementoNoFinal("Matheus");
 		lista.adicionarElementoNoFinal("Paulo");
-		
-		assertEquals(3, lista.pegaTotalElementos());
+		assertEquals(3, lista.getTotalCelulas());
 		
 		lista.removerCelulaPosicaoEspecifica(1);
-		
-		assertEquals(2, lista.pegaTotalElementos());
-		assertEquals("Paulo", lista.pega(1));
-		assertEquals("Gabriel", lista.pegaPrimeiro());
-		assertEquals("Paulo", lista.pegaUltima());
+		assertEquals(2, lista.getTotalCelulas());
+		assertEquals("Paulo", lista.getElementoPosicaoEspecifica(1));
+		assertEquals("Gabriel", lista.getPrimeiraCelula());
+		assertEquals("Paulo", lista.getUltimaCelula());
 	}
 	
 	@Test
 	void testRemoverNegativa() {
-		ListaLigada lista = new ListaLigada();
-		
 		lista.adicionarElementoNoFinal("Gabriel");
 		lista.adicionarElementoNoFinal("Matheus");
 		lista.adicionarElementoNoFinal("Paulo");
@@ -320,8 +250,6 @@ class ListaLigadaTest {
 	
 	@Test
 	void testRemoverPosicaoSuperiorAoTotal() {
-		ListaLigada lista = new ListaLigada();
-		
 		lista.adicionarElementoNoFinal("Gabriel");
 		lista.adicionarElementoNoFinal("Matheus");
 		lista.adicionarElementoNoFinal("Paulo");
